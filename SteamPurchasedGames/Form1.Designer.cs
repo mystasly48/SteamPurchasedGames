3fc274fe6d820766c0ffcc1b92a850ebf84bb818
Maisu <mystasly48@gmail.com>
2019-01-19 05:53:05 +0900

    First commit

--- SteamPurchasedGames/Form1.Designer.cs
+++ SteamPurchasedGames/Form1.Designer.cs
@@ -23,13 +23,99 @@
     /// コード エディターで変更しないでください。
     /// </summary>
     private void InitializeComponent() {
-      this.components = new System.ComponentModel.Container();
+      this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      this.date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      this.title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      this.method = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // richTextBox1
+      // 
+      this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+      this.richTextBox1.Location = new System.Drawing.Point(12, 48);
+      this.richTextBox1.Name = "richTextBox1";
+      this.richTextBox1.Size = new System.Drawing.Size(340, 390);
+      this.richTextBox1.TabIndex = 0;
+      this.richTextBox1.Text = "";
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(12, 12);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "View";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.AllowUserToAddRows = false;
+      this.dataGridView1.AllowUserToDeleteRows = false;
+      this.dataGridView1.AllowUserToOrderColumns = true;
+      this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.date,
+            this.title,
+            this.method});
+      this.dataGridView1.Location = new System.Drawing.Point(359, 48);
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.ReadOnly = true;
+      this.dataGridView1.RowTemplate.Height = 21;
+      this.dataGridView1.Size = new System.Drawing.Size(429, 390);
+      this.dataGridView1.TabIndex = 2;
+      // 
+      // date
+      // 
+      this.date.HeaderText = "Date";
+      this.date.Name = "date";
+      this.date.ReadOnly = true;
+      this.date.ToolTipText = "The date of purchased or activated the game.";
+      // 
+      // title
+      // 
+      this.title.HeaderText = "Title";
+      this.title.Name = "title";
+      this.title.ReadOnly = true;
+      this.title.ToolTipText = "The name of the game.";
+      // 
+      // method
+      // 
+      this.method.HeaderText = "Method";
+      this.method.Name = "method";
+      this.method.ReadOnly = true;
+      this.method.ToolTipText = "Complimentary.";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "Form1";
+      this.Controls.Add(this.dataGridView1);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.richTextBox1);
+      this.Name = "Form1";
+      this.Text = "                                                            ";
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+      this.ResumeLayout(false);
+
     }
 
     #endregion
+
+    private System.Windows.Forms.RichTextBox richTextBox1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.DataGridView dataGridView1;
+    private System.Windows.Forms.DataGridViewTextBoxColumn date;
+    private System.Windows.Forms.DataGridViewTextBoxColumn title;
+    private System.Windows.Forms.DataGridViewTextBoxColumn method;
   }
 }
 
--- SteamPurchasedGames/Form1.cs
+++ SteamPurchasedGames/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,5 +14,70 @@ namespace SteamPurchasedGames {
     public Form1() {
       InitializeComponent();
     }
+
+    private void button1_Click(object sender, EventArgs e) {
+      button1.Enabled = false;
+      Game[] games = ExtractGamesFromHtml(richTextBox1.Text);
+      foreach (Game game in games) {
+        dataGridView1.Rows.Add(game.Date.ToShortDateString(), game.Title, game.Method);
+      }
+      button1.Enabled = true;
+    }
+
+    private Game[] ExtractGamesFromHtml(string html) {
+      List<Game> games = new List<Game>();
+      // <div class="free_license_remove_link"><a href="javascript:RemoveFreeLicense(なんたらかんたら);">（なんたらかんたら）</a></div>
+      html = Regex.Replace(html, @"<div class=""free_license_remove_link"">.*?</div>", "", RegexOptions.Singleline);
+
+      MatchCollection items = Regex.Matches(html,
+        @"<tr>\s*?<td class=""license_date_col"">(?<date>.*?)</td>\s*?<td>(?<title>.*?)</td>\s*?<td class=""license_acquisition_col"">(?<method>.*?)</td>\s*?</tr>",
+        RegexOptions.Singleline);
+      foreach (Match item in items) {
+        Console.WriteLine("Date: {0}, Title: {1}, Method: {2}", item.Groups["date"].Value, item.Groups["title"].Value, item.Groups["method"].Value);
+        games.Add(new Game() {
+          Date = DateTime.Parse(item.Groups["date"].Value.Trim()),
+          Title = item.Groups["title"].Value.Trim(),
+          //Method = ToMethod(item.Groups["method"].Value.Trim())
+          Method = item.Groups["method"].Value.Trim()
+        });
+      }
+      return games.ToArray();
+    }
+
+    //private Method ToMethod(string method) {
+    //  switch (method) {
+    //    case "Steam ストア":
+    //      return Method.Store;
+    //    case "小売店":
+    //      return Method.Retail;
+    //    case "ギフト／ゲストパス":
+    //      return Method.Gift;
+    //    case "無料サービス":
+    //      return Method.Complimentary;
+    //    default:
+    //      throw new ArgumentException();
+    //  }
+    //}
   }
+
+  public class Game {
+    public DateTime Date; // license_date_col
+    public string Title;
+    // you have to search all the items from Title to know the app id.
+    // it means this processing takes a long time and i dont recommend.
+    // by the way is there any library that can get the appid from title fast.
+    //public long Id; 
+    //public Method Method; // license_acquisition_col
+    public string Method;
+  }
+
+  //public enum Method {
+  //  /* にほんごのみたいおうしています This system only supports Japanese html.
+  //   * Store: "Steam Store", "Steam ストア"
+  //   * Retail: "Retail", "小売店"
+  //   * Gift: "Gift/Guest Pass ", "ギフト／ゲストパス"
+  //   * Free: "Complimentary", "無料サービス"
+  //   */
+  //  Store, Retail, Gift, Complimentary
+  //}
 }
